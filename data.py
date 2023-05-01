5099fb05e752d9d3c6e3294b542855508891760c
Gaultier Delbarre <gaultiertexas@gmail.com>
2023-05-01 14:02:36 -0500

    Added first preprocessing steps

--- data.py
+++ data.py
@@ -6,15 +6,32 @@ from torch.nn.utils.rnn import pad_sequence
 
 
 def load_data(x_path):
-    # Your code here
-    return x
+    return pd.read_csv(x_path, low_memory=False)
 
 
-def split_data(x, y, split=0.8):
-    # Your code here
-    return train_x, train_y, test_x, test_y
+def split_data(x, y, split=0.8, random_state=42):
+    # This assumes that X has been preprocessed and its size normalized to be the
+    # same as Y
+
+
+    nval_train = int(x.shape[0] * split)
+    print(f"X: {x.shape}, Y: {y.shape}")
+
+    np.random.seed(random_state)
+
+    idxs = np.random.permutation(x.shape[0] - 1)
+
+    idx_x_train = idxs[:nval_train]
+
+    idx_test = idxs[nval_train:]
+
+    return x.iloc[idx_train], y.iloc[idx_train], x.iloc[idx_test], y.iloc[idx_test]
 
 
 def preprocess_x(df):
-    # Your code here
-    return data
+    patientcounts = df.groupby('patientunitstayid')[['patientunitstayid']]\
+            .transform('count')
+
+    df['counts'] = patientcounts
+
+    return df
